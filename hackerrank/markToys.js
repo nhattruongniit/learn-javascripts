e1ac989b1079dd936aa9598ee907be00f6fb7451
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2019-12-12 23:51:32 +0700

    markToy

---
+++ hackerrank/markToys.js
@@ -0,0 +1,20 @@
+/* Practice -> Interview Preparation Kit -> Sorting -> Mark and Toys */
+
+const prices = [3, 7, 2, 9, 4].sort((a, b) => a - b);
+const budget = 15;
+const toys = [];
+
+const maxiumToys = (prices, budget) => {
+  let num = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if( prices[i] <= budget) {
+      num++;
+
+      budget -= prices[i];
+      console.log(budget)
+    }
+  }
+  console.log(num)
+};
+
+const result = maxiumToys(prices, budget);
