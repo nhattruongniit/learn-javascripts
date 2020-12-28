8f2c7c59476d526998d326f8685dc7f781478b70
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2020-12-28 22:36:08 +0700

    apply two sum

--- leetcode/two-sums.js
+++ leetcode/two-sums.js
@@ -3,8 +3,9 @@
 const input = [2, 2, 3, 4];
 // because 2 + 4 = 6
 
+// way 1
 const twoSum = function (nums, target) {
-  const remainders = {};
+  const remainders = {};  
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
     if (num in remainders) {
@@ -16,4 +17,26 @@ const twoSum = function (nums, target) {
 
 const result = twoSum(input, 6);
 
-console.log("=====result=====", result);
+console.log("result way 1: ", result);
+
+
+const twoSum2 = function (nums, target) {
+  const map = new Map();  
+  for (let i = 0; i < nums.length; i++) {
+    // map.set(key, value)
+    // map.has(key) map.get(key)
+
+    const remain = target - nums[i];
+    if(map.has(remain)) {
+      return [map.get(remain), i]
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+};
+
+
+const result2 = twoSum(input, 6);
+
+console.log("result way 2: ", result2);
