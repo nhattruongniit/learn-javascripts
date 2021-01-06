620ec60f1972e7a4b548b5ad6c49ce1c4643948e
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2021-01-06 19:52:04 +0700

    searchbYName

---
+++ leetcode/searchByName.js
@@ -0,0 +1,29 @@
+const array = [
+  {
+    id: 0,
+    name: 'truong'
+  },
+  {
+    id: 1,
+    name: 'khoa'
+  },
+  {
+    id: 2,
+    name: 'nam'
+  },
+  {
+    id: 3,
+    name: 'thu'
+  }
+]
+
+
+function searchByName(keyword) {
+  const newArray = array.filter(item => item.name.toLocaleLowerCase().indexOf(keyword) !== -1);
+
+  return newArray;
+}
+
+const res = searchByName('t')
+
+console.log('result: ', res)
