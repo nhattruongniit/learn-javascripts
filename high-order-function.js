913741d746ea1d8a631de725b0edd468b1d1a4ce
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2021-05-16 21:22:55 +0700

    hof

--- high-order-function.js
+++ high-order-function.js
@@ -37,3 +37,42 @@ var dogsAdvance = animals.filter(isDog);
 var catAdvance = animals.filter(isCat);
 console.log('advance function dogs: ', dogsAdvance);
 console.log('advance function cat: ', catAdvance);
+
+
+/* ========== grade ======= 
+Tìm thứ hạng trung bình của cả lớp 
+Tìm thứ hạng trung bình của nam trong lớp 
+Tìm thứ hạng trung bình của Nữ trong lớp 
+Tìm thứ hạng cao nhất của Nam trong lớp 
+Tìm thứ hạng cao nhất của Nữ trong lớp
+ Tìm thứ hạng thấp nhất của Nam trong lớp 
+Tìm thứ hạng thấp nhất của Nữ trong lớp 
+Tìm thứ hạng cao nhất của cả lớp 
+Tìm thứ hạng thấp nhất của cả lớp
+*/
+const grades = [
+  {name: 'John', grade: 8, sex: 'M'},
+  {name: 'Sarah', grade: 12, sex: 'F'},
+  {name: 'Bob', grade: 16, sex: 'M'},
+  {name: 'Johnny', grade: 2, sex: 'M'},
+  {name: 'Ethan', grade: 4, sex: 'M'},
+  {name: 'Paula', grade: 18, sex: 'F'},
+  {name: 'Donald', grade: 5, sex: 'M'},
+  {name: 'Jennifer', grade: 13, sex: 'F'},
+  {name: 'Courtney', grade: 15, sex: 'F'},
+  {name: 'Jane', grade: 9, sex: 'F'}
+];
+
+const isMale = student => student.sex === 'M';
+const isFemale = student => student.sex = 'F';
+
+const getMales = grades => grades.filter(isMale);
+const getFemales = grades => grades.filter(isFemale);
+
+const averageLesson = grades => {
+  return grades.reduce((acc, curr) => {
+    acc + curr.grade
+  }, 0) / grades.length;
+}
+
+const maxGrade = grades => Math.max(...grades.map(student => student.grade))
