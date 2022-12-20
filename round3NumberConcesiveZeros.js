7ecd8049d8bdf3db023be8c79d45076f86547b22
Tony Nguyen <truong.nn@amanotes.com>
2022-12-20 11:23:53 +0700

    abc

--- round3NumberConcesiveZeros.js
+++ round3NumberConcesiveZeros.js
@@ -1,12 +1,12 @@
-const input = [0.00000604379583, 0.03506432, 0.00754373]
-// output = [0.00000604, 0.035, 0.00754]
+const input = [0.00000604379583, 0.03056432, 0.007054373]
+// output = [0.00000604, 0.0356, 0.00754]
 
 // how to map input to output
 const res = input.map(num => {
   const str = num.toString()
   const res = str.match(/0/g)
   const len = res.length
-  const str2 = str.slice(0, len + 3)
+  const str2 = str.slice(0, len + 4)
   return str2
 }
 )
