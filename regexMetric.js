486a47cfe60a0efc338752dd3994aa31b5316998
Tony Nguyen <truong.nn@amanotes.com>
2022-12-20 20:27:04 +0700

    pssh

--- regexMetric.js
+++ regexMetric.js
@@ -1,12 +1,26 @@
-const input = ['cm01__fullads_show', 'song_start__song_play_type__replay', 'bannerads']
+const listOfMetrics = ['fullads', 'videoads', 'song_result']
+const metricList = [
+  { displayName: "user_count" },
+  { displayName: "fullads_show_ready" },
+  { displayName: "videoads_show__reward__revive" },
+  { displayName: "song_result__song_play_type__replay" },
+]
 
-// result = 'song_start'
+/*
+output = [
+  { displayName: "user_count" },
+  { displayName: "fullads_show_ready", metric: "fullads" },
+  { displayName: "videoads_show__reward__revive", metric: "videoads" },
+  { displayName: "song_result__song_play_type__replay", metric: "song_result" },
+]
 
-// write match function input  match with 'song_start'
+*/
 
-const abc = input.find(str => {
-  const res = str.match(/song_start/g)
-  return res
-} )
+// map listOfMetrics and metricList to output
+const output = metricList.map(metric => {
+  const { displayName } = metric
+  const metricName = listOfMetrics.find(metric => displayName.includes(metric))
+  return { ...metric, metric: metricName }
+})
+console.log(output)
 
-console.log(abc)
