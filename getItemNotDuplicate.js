9ba3f58e8b603366504d8f82c250431dcde67c79
truong.nn <truong.nn@amanotes.com>
2023-02-15 11:44:17 +0700

    da

--- getItemNotDuplicate.js
+++ getItemNotDuplicate.js
@@ -1,27 +1,26 @@
-const oldCampaigns = [
+const sessionCampaigns = [
   {
     campaignId: "google-195b4eee-b0ee-4475-a70e-6bb8dbafd748",
     campaignName: "ad group 1",
   },
 ];
 
-const newCampaigns = [
-  {
-    campaignId: "google-195b4eee-b0ee-4475-a70e-6bb8dbafd748",
-    campaignName: "ad group 1",
-  },
+const dataSet = [
   {
     campaignId: "google-32231",
     campaignName: "ad group 2",
   },
+  {
+    campaignId: "google-195b4eee-b0ee-4475-a70e-6bb8dbafd748",
+    campaignName: "ad group 1",
+  },
 ];
 
 // get campaign not duplicate in oldCampaigns and newCampaigns
-const resCampaigns = newCampaigns.filter(
-  (newCampaign) =>
-    !oldCampaigns.find(
-      (oldCampaign) => oldCampaign.campaignId === newCampaign.campaignId
-    )
+const resCampaigns = [...dataSet, ...sessionCampaigns].filter(
+  (campaign, index, self) =>
+    index ===
+    self.findIndex((_self) => _self.campaignId === campaign.campaignId)
 );
 
-console.log("google : ", { oldCampaigns, newCampaigns, resCampaigns });
+console.log("res : ", resCampaigns);
--- isRangeValidate.js
+++ isRangeValidate.js
@@ -1,7 +1,17 @@
+const ratio = 1;
+const minRatio = 1;
+const maxRatio = Number.MAX_SAFE_INTEGER;
+
+const minDimension = 200;
+const maxDimension = Number.MAX_SAFE_INTEGER;
+
 const inRange = (number, start, end) => {
   return number >= start && number <= end;
 };
 
-const res = inRange(1200, 0, 9007199254740991);
+const res =
+  inRange(ratio, minRatio, maxRatio) &&
+  (inRange(1080, minDimension, maxDimension) ||
+    inRange(1080, minDimension, maxDimension));
 
 console.log("res: ", res);
