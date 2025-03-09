da6d3db419b6d30f07788d19e28a9d682964721c
Tony Nguyen <nhattruong1811@gmail.com>
2025-03-09 11:55:04 +0700

    Update high-order-function.js

--- high-order-function.js
+++ high-order-function.js
@@ -54,16 +54,61 @@ const grades = [
   {name: 'Jane', grade: 9, sex: 'F'}
 ];
 
-const isMale = student => student.sex === 'M';
-const isFemale = student => student.sex = 'F';
+const isMale = (grade) => grade.sex == 'M';
+const isFemale = (grade) => grade.sex == "F";
+const getBoys = (dataSource = []) => dataSource.filter(isMale);
+const getGirls = (dataSource = []) => dataSource.filter(isFemale);
+const menGrades = grades.filter(isMale);
+const womenGrades = grades.filter(isFemale);
 
-const getMales = grades => grades.filter(isMale);
-const getFemales = grades => grades.filter(isFemale);
+const average = (dataSource = []) => dataSource.reduce((acc, curr) => {
+  return acc + curr.grade;
+}, 0) / grades.length;
 
-const averageLesson = grades => {
-  return grades.reduce((acc, curr) => {
-    acc + curr.grade
-  }, 0) / grades.length;
-}
+const maxGradesCommon = (dataSource = []) => dataSource.reduce((acc, curr) => {
+  return acc.grade > curr.grade ? acc : curr;
+}, {} )
+
+const minGradesCommon = (dataSource = []) => dataSource.reduce((acc, curr) => {
+  return acc.grade < curr.grade ? acc : curr;
+}, {} )
+
+// tính trung binh
+const averageGrades = average(grades)
+
+// trung bình nam trong lớp 
+const averageMenGrades = average(getBoys(grades));
+
+// trung bình nữ trong lớp 
+const averageWomenGrades = average(womenGrades);
+
+// cao nhất lớp
+const maxGrades = maxGradesCommon(grades);
+
+// cao nhất nam trong lớp
+const maxMenGrades = maxGradesCommon(menGrades)
+
+// cao nhất nữ trong lớp
+const maxWomenGrades = maxGradesCommon(womenGrades)
+
+// thấp nhất nam trong lớp
+const minMenGrades = minGradesCommon(menGrades)
+
+ // thấp nhất nữ trong lớp
+const minWomenGrades = minGradesCommon(womenGrades)
+
+// thấp nhất lớp
+const minGrade = minGradesCommon(grades);
+
+console.log('class: ', {
+  averageGrades,
+  maxGrades,
+  minGrade,
+  averageMenGrades,
+  averageWomenGrades,
+  maxMenGrades,
+  minWomenGrades,
+  minMenGrades
+})
 
 const maxGrade = grades => Math.max(...grades.map(student => student.grade))
